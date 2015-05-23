d0ca88f3ed7b18e86c1885817463322d3d96eefb
Eito Katagiri <eitoball@gmail.com>
2015-05-23 16:57:10 +0900

    add comment component (Using props)

--- react_phoenix/web/static/js/comment.react.js
+++ react_phoenix/web/static/js/comment.react.js
@@ -1,3 +1,16 @@
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
 var CommentList = React.createClass({
   render: function() {
     return (
